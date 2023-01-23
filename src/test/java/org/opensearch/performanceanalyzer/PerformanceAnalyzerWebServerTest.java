b74944a27a6028daacdb1951af941991d5f090c5
Filip Drobnjaković <drobnjakovicfilip@gmail.com>
2023-01-23 21:03:33 +0100

    fix and include web server test (#273)
    Signed-off-by: Filip Drobnjakovic <drobnjakovicfilip@gmail.com>

--- src/test/java/org/opensearch/performanceanalyzer/PerformanceAnalyzerWebServerTest.java
+++ src/test/java/org/opensearch/performanceanalyzer/PerformanceAnalyzerWebServerTest.java
@@ -12,6 +12,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.SocketException;
 import java.net.URL;
 import java.security.KeyStore;
 import java.util.Objects;
@@ -341,7 +342,7 @@ public class PerformanceAnalyzerWebServerTest {
                     "tls/attacker/attack_key.pem",
                     "tls/rootca/RootCA.pem");
             throw new AssertionError("An unauthenticated client was able to talk to the server");
-        } catch (SSLException e) { // Unauthenticated client is rejected!
+        } catch (SSLException | SocketException e) { // Unauthenticated client is rejected!
             assert true;
         } catch (Exception e) { // Treat unexpected errors as a failure
             throw new AssertionError(
