e51da5a60a52ee10b7bc0747b74a6bdb3837a8c4
CoderJeffrey <jliu5021@usc.edu>
2023-07-10 14:56:02 -0700

    Update SearchBackPressureAction (Signed-off-by: Jeffrey Liu ujeffliu@amazon.com)
    Signed-off-by: CoderJeffrey <jliu5021@usc.edu>

--- src/main/java/org/opensearch/performanceanalyzer/decisionmaker/actions/SearchBackPressureAction.java
+++ src/main/java/org/opensearch/performanceanalyzer/decisionmaker/actions/SearchBackPressureAction.java
@@ -32,8 +32,9 @@ public class SearchBackPressureAction extends SuppressibleAction {
 
     /* TO DO: Discuss the default cool off period for SearchBackPressureAction
      * Time to wait since last recommendation, before suggesting this action again
+     * Needs the action config to have the cool off period for all dimension
      */
-    private static final long DEFAULT_COOL_OFF_PERIOD_IN_MILLIS = TimeUnit.DAYS.toMillis(3);
+    private static final long DEFAULT_COOL_OFF_PERIOD_IN_MILLIS = TimeUnit.HOURS.toMillis(1);
 
     // step size in percent
     /* From Config Per Diumension Type
@@ -41,7 +42,6 @@ public class SearchBackPressureAction extends SuppressibleAction {
      *  Step Size in percentage
      *  NOT to Node Level but for whole service (so all data node instances)
      *  canUpdate means whether the action should be emitted
-     *
      */
     private final String searchbpDimension;
     private final double desiredValue;
@@ -87,13 +87,16 @@ public class SearchBackPressureAction extends SuppressibleAction {
                 .collect(Collectors.toList());
     }
 
-    /* TO DO: Discuss the impact of SearchBackPressureAction  */
+    /* TO DO: Discuss the impact of SearchBackPressureAction  
+     * since our action only modify the threhsold settings of Search Back Pressure Service instead of actual Resource
+     * No Impact should be put as the Impact Vector for this action so other actions would not be affected by Searchbp-specific actions
+    */
     @Override
     public Map<NodeKey, ImpactVector> impact() {
         Map<NodeKey, ImpactVector> impact = new HashMap<>();
         for (NodeKey key : impactedNodes()) {
             /* TODO: Impact Logic for SearchBackPressureAction */
-            ImpactVector impactVector = new ImpactVector();
+            // ImpactVector impactVector = new ImpactVector();
             impact.put(key, NO_IMPACT);
         }
         return impact;
@@ -119,13 +122,59 @@ public class SearchBackPressureAction extends SuppressibleAction {
         return summary.toJson();
     }
 
-    /* Write Static Class Summary to conver the Action POJO to JSON Object
+    public static final class Builder {
+        private final AppContext appContext;
+        private final String searchbpDimension;
+        private Double currentValue;
+        private Double desiredValue;
+        private long coolOffPeriodInMillis;
+
+        private Builder(
+                final AppContext appContext,
+                final String searchbp_dimension,
+                final long coolOffPeriodInMillis) {
+            this.appContext = appContext;
+            this.searchbpDimension = searchbp_dimension;
+            this.coolOffPeriodInMillis = coolOffPeriodInMillis;
+        }
+
+        public Builder currentValue(Double currentValue) {
+            this.currentValue = currentValue;
+            return this;
+        }
+
+        public Builder desiredValue(Double desiredValue) {
+            this.desiredValue = desiredValue;
+            return this;
+        }
+
+        public Builder coolOffPeriodInMillis(long coolOffPeriodInMillis) {
+            this.coolOffPeriodInMillis = coolOffPeriodInMillis;
+            return this;
+        }
+
+        public SearchBackPressureAction build() {
+            Boolean canUpdate = false;
+            /* 
+             * if desiredValue is between 0 and 100 then canUpdate is true
+             * since desiredValue is valid */
+            if (desiredValue != null) {
+                canUpdate = ((desiredValue >= 0) && (desiredValue <= 100));
+            }
+
+            return new SearchBackPressureAction(
+                    appContext, canUpdate, coolOffPeriodInMillis, searchbpDimension, desiredValue, currentValue);
+        }
+    }
+
+
+    /* Write Static Class Summary to conver the Searchbp Action POJO to JSON Object
      * Key fields to be included
-     * 1. Dimension (name) of the setting to be modified
-     * 2. CurrentValue of the setting to be modified
-     * 2. DesiredValue of the setting to be modified
-     * 3. CoolOffPeriodInMillis
-     * 4. canUpdate (whether the action should be emitted)
+     *  1. Dimension (name) of the Searchbp setting to be modified
+     *  2. CurrentValue of the setting 
+     *  2. DesiredValue of the setting 
+     *  3. CoolOffPeriodInMillis for the action
+     *  4. canUpdate (whether the action should be emitted)
      */
     public static class Summary {
         public static final String SEARCHBP_SETTING_DIMENSION = "searchbp_setting_dimension";
