6eb937a0a629f986185e3dcb8b9d0852fdd5fe16
CoderJeffrey <jliu5021@usc.edu>
2023-07-10 16:16:14 -0700

    Add Searchbp Decider (Signed-off-by: Jeffrey Liu ujeffliu@amazon.com)
    Signed-off-by: CoderJeffrey <jliu5021@usc.edu>

---
+++ src/main/java/org/opensearch/performanceanalyzer/decisionmaker/deciders/searchbackpressure/SearchBackPressureDecider.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.performanceanalyzer.decisionmaker.deciders.searchbackpressure;
+
+
+import java.util.List;
+import org.opensearch.performanceanalyzer.AppContext;
+import org.opensearch.performanceanalyzer.decisionmaker.actions.Action;
+import org.opensearch.performanceanalyzer.decisionmaker.deciders.Decider;
+import org.opensearch.performanceanalyzer.decisionmaker.deciders.Decision;
+import org.opensearch.performanceanalyzer.decisionmaker.deciders.jvm.old_gen.OldGenDecisionPolicy;
+import org.opensearch.performanceanalyzer.decisionmaker.deciders.jvm.sizing.HeapSizeIncreasePolicy;
+import org.opensearch.performanceanalyzer.rca.framework.core.RcaConf;
+import org.opensearch.performanceanalyzer.rca.store.rca.HighHeapUsageClusterRca;
+import org.opensearch.performanceanalyzer.rca.store.rca.jvmsizing.LargeHeapClusterRca;
+import org.opensearch.performanceanalyzer.rca.store.rca.searchbackpressure.SearchBackPressureClusterRCA;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/** decider to change the dynamic settings of SearchBackPressure In-flight Cancellation*/
+public class SearchBackPressureDecider extends Decider {
+    private static final Logger LOG = LogManager.getLogger(SearchBackPressureDecider.class);
+    public static final String NAME = "SearchBackPressureDecider";
+
+    /* TO ADD: SearchBackPressureDecider should have SeachBackPressurePolicy able to evaluate the search back pressure */
+
+    private int currentIteration = 0;
+    private SearchBackPressureClusterRCA searchBackPressureClusterRCA;
+
+    public SearchBackPressureDecider(
+        long evalIntervalSeconds,
+        int decisionFrequency, 
+        SearchBackPressureClusterRCA searchBackPressureClusterRCA
+    ){
+        super(evalIntervalSeconds, decisionFrequency, searchBackPressureClusterRCA);
+        this.searchBackPressureClusterRCA = searchBackPressureClusterRCA;
+        LOG.info("SearchBackPressureDecider created");
+    }
+
+    @Override
+    public String name() {
+        return NAME;
+    }
+
+    @Override
+    public Decision operate() {
+        LOG.info("SearchBackPressureDecider operate() with currentIteration: {}", currentIteration);
+
+        Decision decision = new Decision(System.currentTimeMillis(), NAME);
+        currentIteration += 1;
+        if (currentIteration < decisionFrequency) {
+            return decision;
+        }
+
+        currentIteration = 0;
+
+        // SearchBackPressure Policy is always accepted
+        // List<Action> searchBackPressureActions = seart.evaluate();
+        // oldGenPolicyActions.forEach(decision::addAction);
+        return decision;
+    }
+
+    /* Read RCA Config to fill the dynamic threshold settings for the SearchBackPressure Service */
+    @Override
+    public void readRcaConf(RcaConf conf) {
+        super.readRcaConf(conf);
+        // oldGenDecisionPolicy.setRcaConf(conf);
+    }
+
+    /* Set AppContext for SearchBackPressurePolicy */
+    @Override
+    public void setAppContext(final AppContext appContext) {
+        super.setAppContext(appContext);
+        // oldGenDecisionPolicy.setAppContext(appContext);
+    }
+
+}
