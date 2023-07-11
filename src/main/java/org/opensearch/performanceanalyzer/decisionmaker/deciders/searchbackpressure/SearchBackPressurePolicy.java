a20d6f01a61dbd095b4b151164261dc18a2fd07c
CoderJeffrey <jliu5021@usc.edu>
2023-07-10 17:29:48 -0700

    Update SearchBackpressure Policy (Signed-off-by: Jeffrey Liu ujeffliu@amazon.com)
    Signed-off-by: CoderJeffrey <jliu5021@usc.edu>

--- src/main/java/org/opensearch/performanceanalyzer/decisionmaker/deciders/searchbackpressure/SearchBackPressurePolicy.java
+++ src/main/java/org/opensearch/performanceanalyzer/decisionmaker/deciders/searchbackpressure/SearchBackPressurePolicy.java
@@ -9,8 +9,12 @@ package org.opensearch.performanceanalyzer.decisionmaker.deciders.searchbackpres
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.opensearch.performanceanalyzer.AppContext;
 import org.opensearch.performanceanalyzer.decisionmaker.actions.Action;
 import org.opensearch.performanceanalyzer.decisionmaker.deciders.DecisionPolicy;
+import org.opensearch.performanceanalyzer.decisionmaker.deciders.configs.searchbackpressure.SearchBackPressurePolicyConfig;
+import org.opensearch.performanceanalyzer.rca.framework.core.RcaConf;
+import org.opensearch.performanceanalyzer.rca.store.rca.searchbackpressure.SearchBackPressureClusterRCA;
 
 /**
  * Decides if the SearchBackPressure threshold should be modified suggests actions to take to
@@ -24,8 +28,26 @@ public class SearchBackPressurePolicy implements DecisionPolicy {
     /* TO DO: Check which settings should be modifed based on search heap shard/task cancellation stats */
     boolean searchTaskHeapThresholdShouldChange;
 
+    private AppContext appContext;
+    private RcaConf rcaConf;
+    private SearchBackPressurePolicyConfig policyConfig;
+    private SearchBackPressureClusterRCA searchBackPressureClusterRCA;
+
+    /* Hourly Alarm frequency threshold */
+    private int hourlyAlarmThreshold;
+
+    /* TODO SearchBackPressurePolicy Alarm Monitor */
+
     @Override
     public List<Action> evaluate() {
         return null;
+    }
+
+    public void setAppContext(AppContext appContext) {
+        this.appContext = appContext;
+    }
+
+    public void setRcaConf(final RcaConf rcaConf) {
+        this.rcaConf = rcaConf;
     }
 }
